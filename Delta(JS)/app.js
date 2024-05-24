94cea47d83bbcb925f1f86efda87178226a149ed
Yash Thakur <yashthakur0421@gmail.com>
2024-05-24 18:27:08 +0530

    Add files via upload

---
+++ Delta(JS)/app.js
@@ -0,0 +1,7 @@
+console.log("Hello World !")
+let pencilprice = 10;
+let earserprice = 5;
+// let output = "The total price is :" + (pencilprice + earserprice) + "Rupees.";
+// let output=`` ------>back ticks
+let output = `The total price is : + ${pencilprice + earserprice} + "Rupees.`;
+console.log(output);
